2da88dda9480ab873f7a668c7a7c2186612c8cad
littlepinkbacon <joris.noel12@gmail.com>
2023-10-26 18:02:46 +0200

    devoirs/workshop

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$hostname = 'localhost'; 
+$database = 'blog'; 
+$username = 'root'; 
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion à la base de données réussie.";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
